bf0cd6fefe02a26e632439159b99dbe80a586236
daniel-squires31 <daniel.squires331@gmail.com>
2022-10-07 15:37:22 -0600

    updated styles of project page and image grid

--- src/presentation/components/imageGrid/ImageGrid.js
+++ src/presentation/components/imageGrid/ImageGrid.js
@@ -1,20 +1,17 @@
 import "./ImageGrid.css";
 
-function ImageGrid({ images = [], imageWidth = "50px", numColumns = 1 }) {
+function ImageGrid({ images = [], numColumns = 1 }) {
   return (
     <div className="image-grid-container">
-      <div className="image-grid-contents">
-        {images.map((imageSrc, index) => {
-          return (
-            <img
-              src={imageSrc}
-              alt={`nft-${index + 1}`}
-              key={`nft-${index + 1}`}
-              style={{ width: imageWidth }}
-            />
-          );
-        })}
-      </div>
+      {images.map((imageSrc, index) => {
+        return (
+          <img
+            src={imageSrc}
+            alt={`nft-${index + 1}`}
+            key={`nft-${index + 1}`}
+          />
+        );
+      })}
     </div>
   );
 }
--- src/presentation/pages/projectPage/ProjectPage.js
+++ src/presentation/pages/projectPage/ProjectPage.js
@@ -1,6 +1,6 @@
 import "./ProjectPage.css";
 import projectWelcomeImg from "../../../assets/images/project_welcome.jpg";
-import projectClosingImg from "../../../assets/images/project_closingimg.jpg";
+import roadmapImg from "../../../assets/images/project_closingimg.jpg";
 import {
   herbCosmos,
   headerMessage,
@@ -15,42 +15,44 @@ import { gatherImagesFromFolder } from "../../../utils/utils";
 
 function ProjectPage() {
   const projectPageNfts = gatherImagesFromFolder(
-    require.context("../../../assets/images", false, /\.(png|jpe?g|svg)$/)
+    require.context(
+      "../../../assets/images/projectPageGrid",
+      false,
+      /\.(png|jpe?g|svg)$/
+    )
   );
 
   return (
     <div className="project-page">
-      <div className="welcome-container">
-        <img className="welcome-img" src={projectWelcomeImg} alt="" />
-      </div>
-      <div className="welcome-header">
-        <strong>WELCOME TO THE HERBS FAM</strong>
+      <div className="project-welcome-container">
+        <div className="project-welcome-img-container">
+          <img src={projectWelcomeImg} alt="" />
+        </div>
+        <div className="project-welcome-header">
+          <strong>WELCOME TO THE HERBS FAM</strong>
+        </div>
       </div>
       <hr />
-      <div className="herbcosmos-container">
-        <div className="herbcosmos">
+      <div className="project-herbcosmos-container">
+        <div className="project-herbcosmos">
           <h1>{headerMessage}</h1>
           <p>{herbCosmos}</p>
         </div>
-        <ImageGrid
-          images={projectPageNfts}
-          imageWidth={"80px"}
-          numColumns={2}
-        />
+        <ImageGrid images={projectPageNfts} numColumns={2} />
       </div>
       <hr />
       <Purchase />
       <hr />
-      <div className="herbfam">
+      <div className="project-herbfam">
         <h1>{herbFam}</h1>
         <p>{herbFamP}</p>
       </div>
       <hr />
-      <div className="roadmap">
+      <div className="project-roadmap">
         <h1>{roadmap}</h1>
         <p>{roadmapP}</p>
+        <img src={roadmapImg} alt="" />
       </div>
-      <img className="closing-img" src={projectClosingImg} alt="" />
     </div>
   );
 }
