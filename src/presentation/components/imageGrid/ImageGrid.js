2460c91af5078c723d2102ddf576551a90ce2e75
Codyvegas <codyjohnson2015@yahoo.com>
2022-10-07 15:26:21 -0700

    fixed image grid styling function

--- src/presentation/components/imageGrid/ImageGrid.js
+++ src/presentation/components/imageGrid/ImageGrid.js
@@ -1,24 +1,22 @@
 import "./ImageGrid.css";
+import { v4 as uuidv4 } from "uuid";
 
 function ImageGrid({ images = [], numColumns = 1, gap = 20 }) {
   const getContainerStyles = () => {
     return {
       gap: gap,
-      gridTemplateColumns: `repeat(${numColumns}, calc(${100 / numColumns} - ${
+      gridTemplateColumns: `repeat(${numColumns}, calc(${100 / numColumns}% - ${
         gap / numColumns
-      }))`,
+      }px))`,
     };
   };
 
   return (
     <div className="image-grid-container" style={getContainerStyles()}>
       {images.map((imageSrc, index) => {
+        const imgKey = uuidv4();
         return (
-          <img
-            src={imageSrc}
-            alt={`nft-${index + 1}`}
-            key={`nft-${index + 1}`}
-          />
+          <img src={imageSrc} alt={`nft-${index + 1}`} key={`nft-${imgKey}`} />
         );
       })}
     </div>
