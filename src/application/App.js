20682741bcb1b5741bb6647d5582acce3ef009ca
Codyvegas <codyjohnson2015@yahoo.com>
2022-10-31 10:54:53 -0700

    add scroll to top component for all pages

--- src/application/App.js
+++ src/application/App.js
@@ -1,10 +1,12 @@
 import AppRoutes from "./Routes";
 import Header from "../presentation/components/header/Header";
 import Footer from "../presentation/components/footer/Footer";
+import ScrollToTop from "./Scroll.js";
 
 function App() {
   return (
     <div className="app">
+      <ScrollToTop />
       <Header />
       <AppRoutes />
       <Footer />
