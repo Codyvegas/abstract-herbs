f4a037e5a9de3ba18c1c573f9127b07a8fe26acd
Codyvegas <codyjohnson2015@yahoo.com>
2022-11-21 19:49:14 -0800

    adjust styling - on going process

--- src/presentation/components/imageGrid/ImageGrid.js
+++ src/presentation/components/imageGrid/ImageGrid.js
@@ -1,5 +1,4 @@
 import "./ImageGrid.css";
-import { v4 as uuidv4 } from "uuid";
 
 function ImageGrid({ images = [], numColumns = 1, gap = 20 }) {
   const getContainerStyles = () => {
@@ -14,9 +13,12 @@ function ImageGrid({ images = [], numColumns = 1, gap = 20 }) {
   return (
     <div className="image-grid-container" style={getContainerStyles()}>
       {images.map((imageSrc, index) => {
-        const imgKey = uuidv4();
         return (
-          <img src={imageSrc} alt={`nft-${index + 1}`} key={`nft-${imgKey}`} />
+          <img
+            src={imageSrc}
+            alt={`nft-${index + 1}`}
+            key={`nft-${imageSrc}`}
+          />
         );
       })}
     </div>
